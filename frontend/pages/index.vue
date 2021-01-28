d7c7daa2bd4b780e98e2472bae5ee74466457e02
Tom O'Callaghan <tomirl2008@gmail.com>
2021-01-28 02:41:30 +0000

    Update index.vue

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,65 +1,47 @@
 <template>
-	<section class="container">
-		<div>
-			<app-logo/>
-			<h1 class="title">
-				django
-			</h1>
-			<h2 class="subtitle">
-				Nuxt.js project
-			</h2>
-			<div class="links">
-				<a
-					href="https://nuxtjs.org/"
-					target="_blank"
-					class="button--green">Documentation</a>
-				<a
-					href="https://github.com/nuxt/nuxt.js"
-					target="_blank"
-					class="button--grey">GitHub</a>
-			</div>
-		</div>
-	</section>
+  <v-flex xs12 sm8 md4>
+    <v-card class="elevation-12">
+      <v-toolbar dark color="primary">
+        <v-toolbar-title>Login</v-toolbar-title>
+      </v-toolbar>
+      <v-form @submit.prevent="submit" novalidate>
+        <v-card-text>
+          <p class="error" v-if="error">{{ error }}</p>
+          <v-text-field prepend-icon="person" name="login" label="Login" type="text" v-model="form.username"></v-text-field>
+          <v-text-field id="password" prepend-icon="lock" name="password" label="Password" type="password" v-model="form.password"></v-text-field>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn type="submit" color="primary">Login</v-btn>
+        </v-card-actions>
+      </v-form>
+    </v-card>
+  </v-flex>
 </template>
 
 <script>
-import AppLogo from '~/components/AppLogo.vue';
-
 export default {
-	'components': {
-		AppLogo
-	}
-};
+  middleware: 'auth',
+  data: () => ({
+    error: null,
+    form: {
+      username: '',
+      password: ''
+    }
+  }),
+  methods: {
+    async submit () {
+      try {
+        await this.$auth.login({ data: this.form })
+        if (this.$auth.hasScope('general')) {
+          this.$nuxt.$router.push('/general')
+        } else if (this.$auth.hasScope('admin')) {
+          this.$nuxt.$router.push('/admin')
+        }
+      } catch (e) {
+        this.error = 'Login failed.'
+      }
+    }
+  }
+}
 </script>
-
-<style>
-.container {
-	min-height: 100vh;
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	text-align: center;
-}
-
-.title {
-	font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-	display: block;
-	font-weight: 300;
-	font-size: 100px;
-	color: #35495e;
-	letter-spacing: 1px;
-}
-
-.subtitle {
-	font-weight: 300;
-	font-size: 42px;
-	color: #526488;
-	word-spacing: 5px;
-	padding-bottom: 15px;
-}
-
-.links {
-	padding-top: 15px;
-}
-</style>
-
