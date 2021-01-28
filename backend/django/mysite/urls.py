e9647515287beabc7c3bdb2f1d62f9f6b2495633
Tom O'Callaghan <tomirl2008@gmail.com>
2021-01-28 02:27:09 +0000

    Update urls.py

--- backend/django/mysite/urls.py
+++ backend/django/mysite/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 # ルーティング
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/auth/', include('djoser.urls')),
+    path('api/auth/', include('djoser.urls.authtoken')),
 ]
